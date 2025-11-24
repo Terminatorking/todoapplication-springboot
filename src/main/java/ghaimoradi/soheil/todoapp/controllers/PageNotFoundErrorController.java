5cdc9ef1cad75babf76d93261443a53fd735cecd
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-11-24 23:39:31 +0330

    handle page not found logic

---
+++ src/main/java/ghaimoradi/soheil/todoapp/controllers/PageNotFoundErrorController.java
@@ -0,0 +1,15 @@
+package ghaimoradi.soheil.todoapp.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SuppressWarnings("all")
+public class PageNotFoundErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "pageNotFound";
+    }
+}
